61eede39b9456632275f825acb368d8b91d36f89
alexkharl <sasha_xarlamov@mail.ru>
2022-10-15 18:59:43 +0300

    commit

--- rtl/decoder_riscv.sv
+++ rtl/decoder_riscv.sv
@@ -1,3 +1,5 @@
+`include "../common/defines_riscv.v"
+
 module decoder_riscv (
   input       [31:0]  fetched_instr_i,
   output  reg [1:0]   ex_op_a_sel_o,      // ?????? ??????? ??????????,
@@ -13,6 +15,54 @@ module decoder_riscv (
   output  reg         jal_o,              // ??? ??????
   output  reg         jalr_o              // 
 );
+
+  wire opcode;
+  wire [2:0] funct3;
+
+  always_comb begin
+    ex_op_a_sel_o     = `OP_A_RS1;   
+    ex_op_b_sel_o     = `OP_B_IMM_I;
+    alu_op_o          = `ALU_ADD;
+    mem_req_o         = 0;
+    mem_we_o          = 0;
+    mem_size_o        = `LDST_B;
+    gpr_we_a_o        = 0;
+    wb_src_sel_o      = `WB_EX_RESULT;
+    illegal_instr_o   = 0;
+    branch_o          = 0;
+    jal_o             = 0;
+    jalr_o            = 0;
+    case( opcode )
+      `LOAD_OPCODE: begin
+        if( funct3 > 5 )
+          illegal_instr_o = 1;
+        else begin
+          ex_op_a_sel_o     = `OP_A_RS1;   
+          ex_op_b_sel_o     = `OP_B_IMM_I;
+          alu_op_o          = `ALU_ADD;
+          mem_req_o         = mem_size_o;
+          mem_we_o          = 0;
+          mem_size_o        = funct3;
+          gpr_we_a_o        = 1;
+          wb_src_sel_o      = `WB_LSU_DATA;
+          illegal_instr_o   = 0;
+          branch_o          = 0;
+          jal_o             = 0;
+          jalr_o            = 0;
+        end
+      end
+          
+//      `MISC_MEM_OPCODE
+//      `OP_IMM_OPCODE  
+//      `AUIPC_OPCODE   
+//      `STORE_OPCODE   
+//      `OP_OPCODE      
+//      `LUI_OPCODE     
+//      `BRANCH_OPCODE  
+//      `JALR_OPCODE    
+//      `JAL_OPCODE     
+//      `SYSTEM_OPCODE  
+  end
 
 // ??? ???? ??? ????????
 
---
+++ sim/tb_decode_riscv_obf.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/15/2022 06:59:29 PM
+// Design Name: 
+// Module Name: tb_decode_riscv_obf
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_decode_riscv_obf(
+
+    );
+endmodule
