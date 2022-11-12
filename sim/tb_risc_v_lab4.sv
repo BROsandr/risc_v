94168b4c1804bd8d3a64dea2257fe83a99fe0b77
alexkharl <sasha_xarlamov@mail.ru>
2022-11-12 15:06:25 +0300

    Testbench fix for NBA

--- sim/tb_risc_v_lab4.sv
+++ sim/tb_risc_v_lab4.sv
@@ -2,14 +2,16 @@
 
 module tb_risc_v_lab4();
 
-  reg clk = 0;
-  reg rst = 0;
+  reg         clk    = 0;
+  reg         rst    = 0;
   
-  reg [15:0] result;
+  reg [15:0]  result;
   
-  reg en = 0;
+  reg         en     = 0;
   
-  reg [2:0] SW = 1;
+  reg [2:0]   SW     = 1;
+  
+  int         number = 62;
   
   risc_v_lab4 risc_v_lab4(
     .clk_i( clk ),
@@ -20,39 +22,33 @@ module tb_risc_v_lab4();
   always
     #10 clk <= !clk;
   
-  initial
-    begin
-      @( posedge clk ); 
-      rst <= 0;
-      @( posedge clk );  
-      rst <= 1;
-      @( posedge clk );  
-      rst <= 0;
-    end
+  task reset;
+    rst <= 0;
+    @( posedge clk );  
+    rst <= 1;
+    @( posedge clk );  
+    rst <= 0;
+  endtask
     
   task input_number( input [11:0] number );
-//    logic [11:0] number_reversed;
-//    for( int i = 0; i < 12; ++i ) begin
-//      number_reversed[i] = number[11 - i];
-//    end
     logic [31:0] instruction;
-    instruction <= { number, 5'b00000, 3'b000, 5'b00001, 7'b0010011 };
     
+    instruction <= { number, 5'b00000, 3'b000, 5'b00001, 7'b0010011 };
+    @( posedge clk );
     { risc_v_lab4.instruction_memory.RAM[0], 
       risc_v_lab4.instruction_memory.RAM[1],
       risc_v_lab4.instruction_memory.RAM[2],
       risc_v_lab4.instruction_memory.RAM[3] } <= { instruction[7:0], instruction[15:8], instruction[23:16], instruction[31:24] };
-  endtask
-  
-  int number = 11;  
+  endtask  
     
   initial
     begin
       input_number( number );
-      @( negedge rst );
       @( posedge clk );
-      
+      reset; 
+      @( posedge clk ); 
       en <= 1;
+      
       $display( "primitive device test" );
       #6000
       if( risc_v_lab4.rf.registers[4] )
