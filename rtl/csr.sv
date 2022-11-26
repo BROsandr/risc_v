ea21323bc95e3139c02837f5d0857bd1895f8454
alexkharl <sasha_xarlamov@mail.ru>
2022-11-26 19:03:35 +0300

    Feat(csr): Реализация мультиплексора

--- rtl/csr.sv
+++ rtl/csr.sv
@@ -15,6 +15,12 @@ module csr(
 
   logic mscratch;
 
+  logic to_mie;
+  logic to_mtvec;
+  logic to_mscratch;
+  logic to_mepc;
+  logic to_mcause;
+
   always_comb
     unique case( A ) inside 
       'h304  : RD = mie_o;
@@ -26,5 +32,21 @@ module csr(
       default: RD = 0;
     endcase
 
+  always_comb
+    unique case( A ) inside 
+      'h304  : to_mie      = OP_i[1] & OP_i[0];
+      'h305  : to_mtvec    = OP_i[1] & OP_i[0];
+      'h340  : to_mscratch = OP_i[1] & OP_i[0]; 
+      'h341  : to_mepc     = OP_i[1] & OP_i[0];
+      'h342  : to_mcause   = OP_i[1] & OP_i[0];
+
+      default: begin
+        to_mie      = 0;
+        to_mtvec    = 0;
+        to_mscratch = 0;
+        to_mepc     = 0;
+        to_mcause   = 0;
+      end
+    endcase
 
 endmodule
