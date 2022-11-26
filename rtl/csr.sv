f18b7d193bee5a04a845a1024437e985ce045c6f
alexkharl <sasha_xarlamov@mail.ru>
2022-11-26 18:39:00 +0300

    Feat: csr ports init

--- rtl/csr.sv
+++ rtl/csr.sv
@@ -1,4 +1,14 @@
 module csr(
+  input clk_i,
+  input  [31:0] mcause_i,
+  input  [31:0] PC_i,
+  input  [11:0] A_i,
+  input  [31:0] WD_i,
+  input  [2:0]  OP_i,
 
+  output [31:0] mie_o,
+  output [31:0] mtvec_o,
+  output [31:0] mepc_o,
+  output [31:0] RD_o
 );
 endmodule
