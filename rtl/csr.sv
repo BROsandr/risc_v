144793e4a6b20c2cd46580bf9e8c8eeed3fb966e
alexkharl <sasha_xarlamov@mail.ru>
2022-11-26 18:50:28 +0300

    Feat(csr): Добавление демультиплексора

--- rtl/csr.sv
+++ rtl/csr.sv
@@ -1,14 +1,30 @@
 module csr(
-  input clk_i,
-  input  [31:0] mcause_i,
-  input  [31:0] PC_i,
-  input  [11:0] A_i,
-  input  [31:0] WD_i,
-  input  [2:0]  OP_i,
+  input               clk_i,
+  input        [31:0] mcause_i,
+  input        [31:0] PC_i,
+  input        [11:0] A_i,
+  input        [31:0] WD_i,
+  input        [2:0]  OP_i,
 
-  output [31:0] mie_o,
-  output [31:0] mtvec_o,
-  output [31:0] mepc_o,
-  output [31:0] RD_o
+  output logic [31:0] mie_o,
+  output logic [31:0] mtvec_o,
+  output logic [31:0] mepc_o,
+  output logic [31:0] mcause_o,
+  output logic [31:0] RD_o
 );
+
+  logic mscratch;
+
+  always_comb
+    unique case( A ) inside 
+      'h304  : RD = mie_o;
+      'h305  : RD = mtvec_o;
+      'h340  : RD = mscratch; 
+      'h341  : RD = mepc_o;
+      'h342  : RD = mcause_o;
+
+      default: RD = 0;
+    endcase
+
+
 endmodule
