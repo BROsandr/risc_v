805bd52bd56d8244cc84597cac8f3c16da898ef5
alexkarl <sasha_xarlamov@mail.ru>
2022-12-07 16:47:36 +0300

    Feat(tb_int): Создание tb и проги для int

---
+++ sim/tb_int_prog.sv
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+
+module tb_miriscv_top();
+  localparam  CLK_PERIOD = 20;
+  localparam  RAM_SIZE   = 512;       // in 32-bit words
+
+  reg         clk        = 0;
+  reg         rst_n      = 0;
+
+  
+  int         number     = 4;
+  
+  reg [15:0]  result;
+
+  logic [31:0] int_req;
+  logic [31:0] int_fin;
+  
+  miriscv_top #(
+    .RAM_SIZE       ( RAM_SIZE           ),
+    .RAM_INIT_FILE  ( "../ram/prog_int.txt"         )
+  ) dut (
+    .clk_i          ( clk                ),
+    .rst_n_i        ( rst_n              ),
+    .int_req_i      ( int_req            ),
+    .int_fin_o      ( int_fin            )
+  );
+
+  
+  always
+    #( CLK_PERIOD / 2 ) clk <= !clk;
+  
+  task reset;
+    rst_n <= 0;
+    @( posedge clk );  
+    rst_n <= 1;
+    @( posedge clk );  
+    rst_n <= 0;
+  endtask
+    
+  task input_number( input [11:0] number, input [4:0] rd = 1 );
+    logic [31:0] instruction;
+    instruction    <= { number, 5'b00000, 3'b000, rd, 7'b0010011 };
+    @( posedge clk );
+    dut.ram.mem[0] <= { instruction[31:24], instruction[23:16], instruction[15:8], instruction[7:0] };
+  endtask  
+
+  task interrupt( input int num );  
+    dut.core.csr.mie_o[num] <= 1;
+    dut.core.csr.mtvec_o <= 10;
+    #2 int_req[num] <= 1;
+  endtask
+    
+  initial
+    begin
+      // input_number( number, 2 );
+      @( posedge clk );
+      reset; 
+      @( posedge clk ); 
+      
+      #500;
+      interrupt( 19 );
+      #2000;
+      $finish;
+    end
+
+endmodule
