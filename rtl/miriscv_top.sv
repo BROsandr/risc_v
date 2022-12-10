6b4e7839454fc2535715c180ee4efd5ac6fe3630
alexkarl <sasha_xarlamov@mail.ru>
2022-12-10 11:54:44 +0300

    Feat(addr_decoder): Интеграция без RDsel

--- rtl/miriscv_top.sv
+++ rtl/miriscv_top.sv
@@ -34,12 +34,17 @@ module miriscv_top
   logic          interrupt;
   logic  [31:0]  mcause;
 
+  logic          we;
+  logic          we_m;
+  logic          req;
+  logic          we_leds;
+  logic          RDsel;
+
   logic  data_mem_valid;
   assign data_mem_valid   = (data_addr_core >= RAM_SIZE) ?  1'b0 : 1'b1;
 
-  assign data_rdata_core  = (data_mem_valid) ? data_rdata_ram : 1'b0;
-  assign data_req_ram     = (data_mem_valid) ? data_req_core : 1'b0;
-  assign data_we_ram      =  data_we_core;
+  assign data_rdata_core  = (data_mem_valid) ? RDsel : 1'b0;
+  assign data_req_ram     = (data_mem_valid) ? req_m : 1'b0;
   assign data_be_ram      =  data_be_core;
   assign data_addr_ram    =  data_addr_core;
   assign data_wdata_ram   =  data_wdata_core;
@@ -53,7 +58,7 @@ module miriscv_top
 
     .data_rdata_i  ( data_rdata_core  ),
     .data_req_o    ( data_req_core    ),
-    .data_we_o     ( data_we_core     ),
+    .data_we_o     ( we_m     ),
     .data_be_o     ( data_be_core     ),
     .data_addr_o   ( data_addr_core   ),
     .data_wdata_o  ( data_wdata_core  ),
@@ -94,5 +99,17 @@ module miriscv_top
     .int_fin_o( int_fin_o ),
     .mcause_o( mcause )
   );
+
+  address_decoder address_decoder(
+    .we_i( we ),
+    .req_i( req ),
+    .addr_i( data_addr_core ),
+    .we_leds_o( we_leds ),
+    .we_m_o( data_we_ram ),
+    .req_m_o( data_req_ram ),
+    .RDsel_o( RDsel )
+  );
+
+
 
 endmodule
