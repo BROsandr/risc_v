91fba901c1eb596e416bcb4597433e2883592891
alexkarl <sasha_xarlamov@mail.ru>
2022-12-14 17:18:58 +0300

    Fix(hex_ctrl): Исправление маски для цифр

--- rtl/hex_ctrl.sv
+++ rtl/hex_ctrl.sv
@@ -13,47 +13,49 @@ module hex_ctrl(
 );
 
   logic [15:0] displayed_number;
+  logic [15:0] displayed_number_masked;
 
-  logic [3:0]  mask;
+  logic [15:0] mask;
+  logic [3:0]  an_enable;
+
+  assign       displayed_number_masked = displayed_number & mask;
 
   always_ff @( posedge clk_i or posedge rst_i )
     if( rst_i )
       displayed_number <= 0;
     else if( we_i && addr_i[3] == 0 ) begin
-      if( be_i == 4'b0001 )
+      if( be_i[0] )
         displayed_number[3:0]   <= wdata_i[3:0];
-      if( be_i == 4'b0010 )  
+      if( be_i[1] )  
         displayed_number[7:4]   <= wdata_i[7:4];
-      if( be_i == 4'b0100 )
+      if( be_i[2] )
         displayed_number[11:8]  <= wdata_i[11:8];
-      if( be_i == 4'b1000 )
+      if( be_i[3] )
         displayed_number[15:12] <= wdata_i[15:12];
     end
 
-  always_ff @( posedge clk_i or posedge rst_i )
-    if( rst_i )
-      displayed_number          <= 0;
-    else begin
-      if( mask == 4'b1110 )
-        displayed_number[3:0]   <= 0;
-      if( mask == 4'b1101 )  
-        displayed_number[7:4]   <= 0;
-      if( mask == 4'b1011 )
-        displayed_number[11:8]  <= 0;
-      if( mask == 4'b0111 )
-        displayed_number[15:12] <= 0;
-    end
+  always_comb begin
+    mask          = { 16 { 1'b1 } };
+    if( !an_enable[0] )
+      mask[3:0]   = 0;
+    if( !an_enable[1] )  
+      mask[7:4]   = 0;
+    if( !an_enable[2] )
+      mask[11:8]  = 0;
+    if( !an_enable[3] )
+      mask[15:12] = 0;
+  end
 
   always_ff @( posedge clk_i or posedge rst_i )
     if( rst_i )
-      mask <= 0;
+      an_enable <= 0;
     else if( we_i && addr_i[3] == 1 )
-      mask <= wdata_i[3:0];
+      an_enable <= wdata_i[3:0];
 
   Seven_segment_LED_Display_Controller Seven_segment_LED_Display_Controller(
     .clock_100Mhz( clk_i ), // 100 Mhz clock source on Basys 3 FPGA
     .reset( rst_i ), // reset
-    .displayed_number_i( displayed_number ),
+    .displayed_number_i( displayed_number_masked ),
     
     .AN( an_o ), // anode signals of the 7-segment LED display
     .SEG( seg_o )// cathode patterns of the 7-segment LED display
