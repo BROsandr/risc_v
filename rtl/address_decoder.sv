40da2da892f7a2baf33d881e6da268f1fbc3fe90
alexkarl <sasha_xarlamov@mail.ru>
2022-12-14 17:17:55 +0300

    Fix(addr_decoder): Исравление адреса под hex

--- rtl/address_decoder.sv
+++ rtl/address_decoder.sv
@@ -20,7 +20,7 @@ module address_decoder #(
   logic  is_hex_addr;
 
   assign is_leds_addr = { addr_i[31:2], 2'b0 } == 32'h80000800;
-  assign is_hex_addr  = { addr_i[31:2], 2'b0 } == 32'h80001000;
+  assign is_hex_addr  = { addr_i[31:4], 4'b0 } == 32'h80001000;
 
   assign data_mem_valid   = ( addr_i >= RAM_SIZE ) ?  ( 1'b0 ) : ( 1'b1 );
 
