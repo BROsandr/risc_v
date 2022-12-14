e159c4165831f8ddb079bf7203a9c6d853436fc6
alexkarl <sasha_xarlamov@mail.ru>
2022-12-14 16:26:20 +0300

    Feat(hex_ctrl): Внедрение регистра отключения hex

--- rtl/hex_ctrl.sv
+++ rtl/hex_ctrl.sv
@@ -14,18 +14,41 @@ module hex_ctrl(
 
   logic [15:0] displayed_number;
 
+  logic [3:0]  mask;
+
   always_ff @( posedge clk_i or posedge rst_i )
     if( rst_i )
       displayed_number <= 0;
-    else if( we_i )
-      case( be_i )
-        4'b0001: displayed_number[3:0] <= wdata_i;
-        4'b0010: displayed_number[7:4] <= wdata_i;
-        4'b0100: displayed_number[11:8] <= wdata_i;
-        4'b1000: displayed_number[15:12] <= wdata_i;
-        default: displayed_number <= 0;
-      endcase
+    else if( we_i && addr_i[3] == 0 ) begin
+      if( be_i == 4'b0001 )
+        displayed_number[3:0]   <= wdata_i[3:0];
+      if( be_i == 4'b0010 )  
+        displayed_number[7:4]   <= wdata_i[7:4];
+      if( be_i == 4'b0100 )
+        displayed_number[11:8]  <= wdata_i[11:8];
+      if( be_i == 4'b1000 )
+        displayed_number[15:12] <= wdata_i[15:12];
+    end
 
+  always_ff @( posedge clk_i or posedge rst_i )
+    if( rst_i )
+      displayed_number          <= 0;
+    else begin
+      if( mask == 4'b1110 )
+        displayed_number[3:0]   <= 0;
+      if( mask == 4'b1101 )  
+        displayed_number[7:4]   <= 0;
+      if( mask == 4'b1011 )
+        displayed_number[11:8]  <= 0;
+      if( mask == 4'b0111 )
+        displayed_number[15:12] <= 0;
+    end
+
+  always_ff @( posedge clk_i or posedge rst_i )
+    if( rst_i )
+      mask <= 0;
+    else if( we_i && addr_i[3] == 1 )
+      mask <= wdata_i[3:0];
 
   Seven_segment_LED_Display_Controller Seven_segment_LED_Display_Controller(
     .clock_100Mhz( clk_i ), // 100 Mhz clock source on Basys 3 FPGA
