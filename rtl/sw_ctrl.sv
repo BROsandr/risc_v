b13d17d17a10fb1a3e9ab11e465c98ca2d5a73b1
alexkarl <sasha_xarlamov@mail.ru>
2022-12-16 16:29:04 +0300

    Style(sw_ctrl): Выравнивание отступов

--- rtl/sw_ctrl.sv
+++ rtl/sw_ctrl.sv
@@ -1,9 +1,9 @@
 module sw_ctrl(
-  input  logic clk_i,
-  input  logic rst_i,
+  input  logic         clk_i,
+  input  logic         rst_i,
 
-  input  logic [15:0] in_i,
-  output logic [31:0] out_o,
+  input  logic  [15:0] in_i,
+  output logic  [31:0] out_o,
 
   output logic  [31:0] int_req_o,
   input  logic  [31:0] int_fin_i
@@ -13,9 +13,9 @@ module sw_ctrl(
   logic         int_fin;
 
   assign        int_req_o[0] = int_req;
-  assign        int_fin = int_fin_i[0];
+  assign        int_fin      = int_fin_i[0];
 
-  assign        out_o[15:0] = in_i;
+  assign        out_o[15:0]  = in_i;
 
   always_ff @( posedge clk_i or posedge rst_i )
     if( rst_i )
